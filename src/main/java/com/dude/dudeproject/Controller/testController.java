277620729007db788b1f772e5dc936a88ce5fd19
Heidish <heidish005@gmail.com>
2020-11-27 12:05:53 +0900

    update [html] [css] [images]

--- src/main/java/com/dude/dudeproject/Controller/testController.java
+++ src/main/java/com/dude/dudeproject/Controller/testController.java
@@ -4,6 +4,7 @@ package com.dude.dudeproject.Controller;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class testController {
